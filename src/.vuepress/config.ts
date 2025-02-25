9ce8f0dd47c51e330654426268420fc91084e396
icelyb24 <icelyb24@163.com>
2025-02-25 23:38:13 +0800

    修改地址路径，添加base: "/tech-blog/",

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/tech-blog/",
 
   lang: "zh-CN",
   title: "博客演示",
