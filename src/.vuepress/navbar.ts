9aaf2cec4c32f656bb879dc6d7a9ae4e71ed21a1
james <james_bambridge@163.com>
2025-02-26 17:14:24 +0800

    修改了icon图标。

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -4,27 +4,27 @@ export default navbar([
   "/",
   {
     text: "生活",
-    icon: "book",
+    icon: "mug-hot",
     prefix: "/posts/",
     children: [
       {
         text: "生活感悟",
-        icon: "pen-to-square",
+        icon: "pencil",
         prefix: "生活感悟/",
         children: [
-          { text: "生活小感慨", icon: "pen-to-square", link: "生活小感慨" },
+          { text: "生活小感慨", icon: "pencil", link: "生活小感慨" },
         ],
       },
     ]
   },
   {
     text: "技术",
-    icon: "pen-to-square",
+    icon: "folder-open",
     prefix: "/posts/",
     children: [
       {
         text: "技术文档",
-        icon: "pen-to-square",
+        icon: "folder-open",
         prefix: "技术文档/",
         children: [
           { text: "关于Helloworld", icon: "pen-to-square", link: "关于Helloworld" },
@@ -35,7 +35,7 @@ export default navbar([
   },
   {
     text: "简介",
-    icon: "book",
+    icon: "address-card",
     prefix: "/",
     link: "intro",
   },
